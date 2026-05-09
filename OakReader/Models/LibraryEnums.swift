0158efb747ead616c8630fdd0d529ffacbd2de6f
Jiwei,Yuan <ji-weiyuan@outlook.com>
2026-05-10 00:01:25 +0100

    refactor(models): add markdown item type and update library models

--- OakReader/Models/LibraryEnums.swift
+++ OakReader/Models/LibraryEnums.swift
@@ -27,8 +27,9 @@ enum SystemCollectionID {
     static let webSnapshots = UUID(uuidString: "00000000-0000-0000-0000-000000000006")!
     static let videos      = UUID(uuidString: "00000000-0000-0000-0000-000000000007")!
     static let recentlyRead = UUID(uuidString: "00000000-0000-0000-0000-000000000008")!
+    static let notes       = UUID(uuidString: "00000000-0000-0000-0000-000000000009")!
 
-    static let all: [UUID] = [allItems, recent, recentlyRead, pdfs, webSnapshots, videos]
+    static let all: [UUID] = [allItems, recent, recentlyRead, pdfs, webSnapshots, videos, notes]
 }
 
 // MARK: - Local user ID
